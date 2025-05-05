d588baf96e2ee039af90f7742c0928742376b3fd
Jynnmei <jynn_mei@yahoo.com>
2025-05-05 23:43:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 // 1. Be Polite, Greet the User
 // Task: Create a route that responds to URLs like /greetings/<username-parameter>.
-
 // Examples: Matches routes like /greetings/Christy or /greetings/Mathilda.
-
 // Response: Include the username from the URL in the response,
 // such as “Hello there, Christy!” or “What a delight it is to see you once more, Mathilda.”
 
@@ -30,12 +28,9 @@ app.get("/greetings/:username", (req, res) => {
 
 // 2. Rolling the Dice
 // Task: Set up a route to handle URLs following the pattern /roll/<number-parameter>.
-
 // Examples: Matches routes like /roll/6 or /roll/20.
-
 // Validation: If the parameter is not a number, respond with “You must specify a number.”
 // For instance, /roll/potato should trigger this response.
-
 // Functionality: If a valid number is provided, respond with a random whole number between 0 and the given number.
 // For example, a request to /roll/16 might respond with “You rolled a 14.”
 
@@ -52,22 +47,12 @@ app.get("/roll/:number", (req, res) => {
 
 // 3. I Want THAT One!
 // Task: Create a route for URLs like /collectibles/<index-parameter>.
-
 // Examples: Matches routes such as /collectibles/2 or /collectibles/0.
-
 // Validation: If the index does not correspond to an item in the array,
 // respond with “This item is not yet in stock. Check back soon!”
-
 // Response: Should describe the item at the given index,
 // like “So, you want the shiny ball? For 5.95, it can be yours!”
 // Include both the name and price properties.
-
-// Data Array:
-//   const collectibles = [
-//   { name: "shiny ball", price: 5.95 },
-//   { name: "autographed picture of a dog", price: 10 },
-//   { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
-// ];
 
 app.get("/collectibles/:index", (req, res) => {
   const index = req.params.index;
